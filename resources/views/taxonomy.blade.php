06dbcf02eccbce83a37f4bb05a5f300602cc4819
Jonathan Roy <jroy66@gmail.com>
2022-10-19 08:47:22 -0400

    dev

--- resources/views/taxonomy.blade.php
+++ resources/views/taxonomy.blade.php
@@ -3,18 +3,24 @@
 @section('content')
     <section class="section">
         <div class="container is-max-desktop">
+            <h3 class="subtitle">
+                {{ $taxonomy->type }}
+            </h3>
+
             <h1 class="title is-1">
                 {{ $taxonomy }}
             </h1>
 
-            @if (isset($taxonomy->entries) && $taxonomy->entries->count() > 0)
+            @if (isset($entries) && $entries->count() > 0)
                 <div class="columns is-multiline">
-                    @foreach ($taxonomy->entries as $entry)
+                    @foreach ($entries as $entry)
                         <div class="column is-4">
                             @include('partials.entry')
                         </div>
                     @endforeach
                 </div>
+
+                {{ $entries->appends(request()->input())->links('partials.pagination') }}
             @else
                 <p>{{ $site->trans('general.noEntries') }}</p>
             @endif
