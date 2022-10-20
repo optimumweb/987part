5c40e0c5dec2a82ecf0e526044a78c94026877b6
Jonathan Roy <jroy66@gmail.com>
2022-10-20 15:48:00 -0400

    dev

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1,11 +1,11 @@
 <nav
     id="menu-{{ $menu->location }}"
-    class="menu"
+    class="site-menu"
     role="navigation"
 >
     <ul>
         @foreach($menu->root_items as $item)
-            @include('menu.item', ['item' => $item, 'depth' => 0])
+            @include('partials.menu-item', ['item' => $item, 'depth' => 0])
         @endforeach
     </ul>
 </nav>
